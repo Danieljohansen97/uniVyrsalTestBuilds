568f3bc3587ea1e7429632f48e197fa646a358fc
Danieljohansen97 <danieljohansen1997@hotmail.com>
2019-12-11 13:40:06 +0100

    Update testbuild-01

---
+++ testbuild-01/js/home.js
@@ -0,0 +1,26 @@
+const input = document.querySelector("#destination");
+const submitBtn = document.querySelector("#submitBtn");
+
+// eventlisteners for disabling/enabling submitBtn
+input.addEventListener("mouseenter", () => {
+    if (input.value !== "") {
+        submitBtn.classList.remove("disabled");
+    } else if (input.value == "") {
+        submitBtn.classList.add("disabled");
+    }
+
+});
+input.addEventListener("mouseleave", () => {
+    if (input.value !== "") {
+        submitBtn.classList.remove("disabled");
+    } else if (input.value == "") {
+        submitBtn.classList.add("disabled");
+    }
+
+});
+
+// submitBtn eventlistener
+submitBtn.addEventListener("click", () => {
+    console.log(input.value);
+});
+
